79eea79aa0987972ca1dec2b3e4487a09aca7b24
vladislawfox <vladislaw.prykhodko@gmail.com>
2019-01-19 23:44:02 +0200

    Add scope annotations

---
+++ base/src/main/java/com/vladislawfox/base/presentation/di/scope/PerController.kt
@@ -0,0 +1,10 @@
+package com.vladislawfox.base.presentation.di.scope
+
+import javax.inject.Scope
+
+/**
+ * Created by vladislawfox on 1/19/19.
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerController
